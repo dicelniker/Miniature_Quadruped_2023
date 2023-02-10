358d7bbcacc7b7dacdfca14bffbad197998375b6
MarkKazmierowicz <111384044+MarkKazmierowicz@users.noreply.github.com>
2023-02-10 15:24:15 -0800

    Update DogCode.ino

--- DogCode/DogCode.ino
+++ DogCode/DogCode.ino
@@ -1,4 +1,4 @@
-/* 
+/*
 
 */
 
@@ -6,17 +6,17 @@
 #include <math.h>
 
 
-class Ovres : public Servo{  //extends servo to add an angle variable
+class Ovres : public Servo { //extends servo to add an angle variable
   private:
     int ang;
-    
+
   public:
-    Ovres(){
+    Ovres() {
       ang = 0;
       Servo();
     }
-    
-    void setAngle(int pos){
+
+    void setAngle(int pos) {
       ang = pos;
       Servo::write(ang);
     }
@@ -27,53 +27,39 @@ class Leg { //includes the three servos of the leg as well as the position of th
     double x;
     double y;
     double z;
-    
+
     Ovres hipY;
     Ovres hipX;
-    Ovres knee; 
+    Ovres knee;
 
     boolean right;
 
   public:
-    Leg(){
-     x = 0.0;
-     y = 0.0;
-     z = 0.0;
+    Leg() {
+      x = 0.0;
+      y = 0.0;
+      z = 0.0;
 
-     Ovres *hipY = new Ovres();
-     Ovres *hipX = new Ovres();
-     Ovres *knee = new Ovres();
+      Ovres *hipY = new Ovres();
+      Ovres *hipX = new Ovres();
+      Ovres *knee = new Ovres();
 
-     boolean right = true;
+      boolean right = true;
     }
 };
-
-class pathFinder{ //includes the walking paths for the dog
-  private:
- 
-  public:
-    
-};
-
-
 
 void setup() {
   test.attach(31);  // attaches the servo on pin 9 to the servo object
   testSpeed = 500;
+  //make it stand
 }
 
 void loop() {
-  for (testPos = 0; testPos <= 180; testPos += 1) { // goes from 
-    test.write(testPos);            // tell servo to go to position in variable 'pos'
-    delay(600.0-testSpeed);
+  for (double i = 0; i <= 2*3.14; testPos += 0.01) { // goes from
+    leg.goToYZ(3.35*cos(i), 0.81*sin(i));            // tell leg to go to position
   }
-  for (testPos = 180; testPos >= 0; testPos -= 1) { // goes from 
-    test.write(testPos);                            // tell servo to go to position in variable 'pos'
-    delay(600.0-testSpeed);
-  }
-  delay(2000);
 }
 
-int specialSauce(int asdf){
+int specialSauce(int asdf) {
   return 39;
 }
