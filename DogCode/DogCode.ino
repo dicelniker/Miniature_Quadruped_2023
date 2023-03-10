6b4d1c36b24920cf9f168e90718e691a6c7e9bff
MarkKazmierowicz <111384044+MarkKazmierowicz@users.noreply.github.com>
2023-03-10 12:10:31 -0800

    Made ready for testing minus constants

--- DogCode/DogCode.ino
+++ DogCode/DogCode.ino
@@ -8,6 +8,7 @@ Dogcode
 
 
 class Leg { //includes the three servos of the leg as well as wether or not the leg is on the right or the left
+    
   private:
   
     Servo hipY;
@@ -17,26 +18,41 @@ class Leg { //includes the three servos of the leg as well as wether or not the 
     boolean right;
 
    public:
-    Leg(int hipYPin, int hipXPin, int kneePin, boolean onRightQuestionMark) {
-      
-      right = onRightQuestionMark;
-
-      Servo *hipY = new Servo();
-      hipY.attach(hipYPin);
-      
-      Servo *hipX = new Servo();
-      hipX.attach(hipXPin);
-      
-      Servo *knee = new Servo();
-      knee.attach(kneePin);
-
-      boolean right = true;
+    Leg(int hipYPin, int hipXPin, int kneePin, boolean onRightQuestionMark);
+    
+    Leg();
+   
+    void setYZ(double hipXAng, double kneeAng){
+        hipX.writeMicroseconds(specialSauce(hipXAng));
+        knee.writeMicroseconds(specialSauce(kneeAng));
     }
 
-    setYZ(int y, int z){
-      
+    void setHip(double ang){
+      hipY.writeMicroseconds(specialSauce(ang));
+    }
+    
+    static double specialSauce(double angle){
+      double result = (angle/160.0)*(2400-550) + 550;
+      return result;
     }
 };
+
+Leg::Leg(int hipYPin, int hipXPin, int kneePin, boolean onRightQuestionMark) {
+      Servo *hipY = new Servo();
+      hipY->attach(hipYPin);
+      
+      Servo *hipX = new Servo();
+      hipX->attach(hipXPin);
+      
+      Servo *knee = new Servo();
+      knee->attach(kneePin);
+
+      right = onRightQuestionMark;
+}
+
+Leg::Leg(){
+  right = false;
+}
 
 class Dog {
   private:
@@ -46,47 +62,49 @@ class Dog {
     Leg RL;
 
     //list<Leg>legList;
-    Leg[] legs;
-    
+    Leg legs[4] = {Leg(9,7,4,false),
+                   Leg(9,7,4,false),
+                   Leg(10,12,5,true),
+                   Leg(11,13,3,false)
+    };    
   public:
-    Dog() {
-      Leg *FL = new Leg(9,7,4,false);      
-      Leg *FR = new Leg(8,6,2,true);
-      Leg *RR = new Leg(10,12,5,true);
-      Leg *RL = new Leg(11,13,3,false);
-
-      Leg[] legs = new Leg[4];
-      legs[0] = FL;
-      legs[1] = FR;
-      legs[2] = RR;
-      legs[3] = RL;
-      //legList.add(FL);
-      //legList.add(FR);
-      //legList.add(RR);
-      //legList.add(RL);
-      
-    }
+    Dog();
+    
     //setLegs(int FLx, int FLy, int FLz, int FRx, int FRy, int FRz,] int RRx, int RRy, int RRz, int RLx, int RLy, int RLz){
-    setLegsYZ(int hipXAng, int kneeAng){
+    void setLegsYZ(double hipXAng, double kneeAng){
       for (Leg temp : legs){
-        temp.setYZ(y,z)
-        }
+        temp.setYZ(hipXAng,kneeAng);
+      }
+    }
+
+    void setHips(double ang){
+      for (Leg temp : legs){
+        temp.setHip(ang); //not sure if this works with the for loop and setting a variable to smthn
       }
     }
 };
 
+Dog::Dog() {
+      //Leg *fl = new Leg(9,7,4,false);      
+      //Leg *fr = new Leg(8,6,2,true);
+      //Leg *rr = new Leg(10,12,5,true);
+      //Leg *rl = new Leg(11,13,3,false);
+
+      //legs[0] = fl;
+      //legs[1] = fr;
+      //legs[2] = rr;
+      //legs[3] = rl;
+}
+
+Dog* skorupi = new Dog();
+
 void setup() {
-  Dog skorupi = new Dog();
-  
+  skorupi->setHips(80.0);
+  skorupi->setLegsYZ(0.0,0.0);
+  delay(5000);
   
 }
 
 void loop() {
-  for (double i = 0; i <= 2 * 3.14; testPos += 0.01) { // goes from
-    leg.goToYZ(3.35 * cos(i), 0.81 * sin(i));        // tell leg to go to position
-  }
-}
-
-int specialSauce(int asdf) {
-  return 39;
+  skorupi->setLegsYZ(0.0,0.0);
 }
